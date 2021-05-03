d81803c3014fe5b58d994ff958aae2752a9cc72e
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-05-03 11:23:13 +0300

    Add individual shed production rate

---
+++ function.js
@@ -0,0 +1,6 @@
+var shedA = 510, shedB = 308, shedC = 486, shedD = 572; 
+
+    console.log("Your production in shed A is"+' '+shedA+' '+"litres per day");
+    console.log("Your production in shed B is"+' '+shedB+' '+"litres per day");
+    console.log("Your production in shed C is"+' '+shedC+' '+"litres per day");
+    console.log("Your production in shed D is"+' '+shedD+' '+"litres per day");
