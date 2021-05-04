48679465c154551683272eac2eb3f2e4dbe40672
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-05-04 10:45:51 +0300

    Add month and days array in javascript file

--- main.js
+++ main.js
@@ -8,7 +8,28 @@ function add(){
     var b = parseInt(document.querySelector("shedB").value);
     var c = parseInt(document.querySelector("shedC").value);
     var d = parseInt(document.querySelector("shedD").value);
-    var sum = a + b + c + d;
-    document.getElementById("sum").innerHTML = sum;
+    var totalProduction = a + b + c + d;
+    document.getElementById("totalProduction").innerHTML = sum;
+
+    return totalProduction;
 }
+
+
+
+
+let milkPricePerLitre = 45;
+let production = add();
+var monthlyEarnings = [{month:"January", days:31}, {month:"February", days:29}, {month:"March", days:31}, {month:"April", days:30}, {month:"May", days:31},
+                        {month:"June", days:30}, {month:"July", days:31}, {month:"August", days:31}, {month:"September", days:30}, {month:"October", days:31}, 
+                        {month:"November", days:30}, {month:"December", days:31}];
+
+function revenues() {
+    var total = 0;
+    for (var i=0; i<monthlyEarnings.length; i++){
+        total = totalProduction * monthlyEarnings * milkPricePerLitre;
+        document.getElementById("")
+    }
+    return total;
+}
+
 
