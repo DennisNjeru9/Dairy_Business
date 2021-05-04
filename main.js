ab27cadb8e0229caaa77dbba83078a752407ed9a
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-05-04 08:42:05 +0300

    Change in js name and update in total production js code

---
+++ main.js
@@ -0,0 +1,13 @@
+var a = document.querySelector("shedA").value;
+var b = document.querySelector("shedB").value;
+var c = document.querySelector("shedC").value;
+var d = document.querySelector("shedD").value;
+
+function add() {
+    var a = parseInt(document.querySelector("shedA").value);
+    var b = parseInt(document.querySelector("shedB").value);
+    var c = parseInt(document.querySelector("shedC").value);
+    var d = parseInt(document.querySelector("shedD").value);
+    var sum = a + b + c + d;
+    document.getElementById("sum").innerHTML = sum;
+}
