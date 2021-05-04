92863ed076f008d62e477a9c7648a49f2cc1a759
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-05-05 00:09:19 +0300

    Add totalProduction function feature

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-var a = document.querySelector("shedA").value;
-var b = document.querySelector("shedB").value;
-var c = document.querySelector("shedC").value;
-var d = document.querySelector("shedD").value;
+var a = document.querySelector("#shedA").value;
+var b = document.querySelector("#shedB").value;
+var c = document.querySelector("#shedC").value;
+var d = document.querySelector("#shedD").value;
 
-function add(){
-    var a = parseInt(document.querySelector("shedA").value);
-    var b = parseInt(document.querySelector("shedB").value);
-    var c = parseInt(document.querySelector("shedC").value);
-    var d = parseInt(document.querySelector("shedD").value);
-    var totalProduction = a + b + c + d;
-    document.getElementById("totalProduction").innerHTML = sum;
-
-    return totalProduction;
+function totalProduction(){
+    var a = parseInt(document.querySelector("#shedA").value);
+    var b = parseInt(document.querySelector("#shedB").value);
+    var c = parseInt(document.querySelector("#shedC").value);
+    var d = parseInt(document.querySelector("#shedD").value);
+    var sum = a + b + c + d;
+    document.getElementById("sum").innerHTML = sum;
 }
 
 
@@ -23,7 +21,7 @@ var monthlyEarnings = [{month:"January", days:31}, {month:"February", days:29}, 
                         {month:"June", days:30}, {month:"July", days:31}, {month:"August", days:31}, {month:"September", days:30}, {month:"October", days:31}, 
                         {month:"November", days:30}, {month:"December", days:31}];
 
-function revenues() {
+function revenues(totalProduction, monthlyEarnings, milkPricePerLitre) {
     var total = 0;
     for (var i=0; i<monthlyEarnings.length; i++){
         total = totalProduction * monthlyEarnings * milkPricePerLitre;
